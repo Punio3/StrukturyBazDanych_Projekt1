df32a49f9baf855f25b21531174c5dc1ef4ca436
Punio3 <czlapset@wp.pl>
2024-11-07 00:02:13 +0100

    Dodana klasa do gromadzenia informacji o operacjach na dysku

---
+++ StrukturyBazDanychC#_Projekt_1/SortingInformations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrukturyBazDanychC__Projekt_1
+{
+    public class SortingInformations
+    {
+        public int countRead = 0;
+        public int countWrite = 0;
+        public int countSplitting = 0;
+        public int countMerge = 0;
+
+        public void DisplayInformations()
+        {
+            Console.WriteLine($"countRead: {countRead}\n" +
+                               $"countWrite: {countWrite}\n" +
+                               $"countRead: {countSplitting}\n" +
+                               $"countRead: {countMerge}\n");
+    }
+    }
+
+}
